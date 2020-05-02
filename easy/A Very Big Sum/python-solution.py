ba1818008c4bf331120640f2d2f08df1c6e2a962
lrgfilipe <luis.filipe@linkconsulting.com>
2020-05-02 04:19:12 +0100

    A Very Big Sum - javascript

--- easy/A Very Big Sum/python-solution.py
+++ easy/A Very Big Sum/python-solution.py
@@ -1,15 +1,1 @@
-#!/bin/python3
-
-import os
-
-# start
 def aVeryBigSum(ar): return sum(ar)
-# end
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-    ar_count = int(input())
-    ar = list(map(int, input().rstrip().split()))
-    result = aVeryBigSum(ar)
-    fptr.write(str(result) + '\n')
-    fptr.close()
--- easy/A Very Big Sum/scala-solution.scala
+++ easy/A Very Big Sum/scala-solution.scala
@@ -1,25 +1,8 @@
-import java.io._
+def aVeryBigSum(ar: Array[Long]): Long = ar.reduce( (a,b) => sum(a.toString, b.toString))
 
-object Solution {
-
-  // start
-  def aVeryBigSum(ar: Array[Long]): Long = ar.reduce( (a,b) => sum(a.toString, b.toString))
-
-  def sum(a: String, b: String, rest: Int = 0, list: List[String] = Nil): Long = {
-    if (a.isEmpty()) return (rest.toString + list.mkString).toLong
-    val f = (a takeRight 1).toInt + (b takeRight 1).toInt + rest
-    if (f < 10) sum(a dropRight 1, b dropRight 1, 0, f.toString :: list)
-    else sum(a dropRight 1, b dropRight 1, f/10, (f%10).toString :: list)
-  }
-  // end
-
-  def main(args: Array[String]) {
-    val stdin = scala.io.StdIn
-    val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
-    val arCount = stdin.readLine.trim.toInt
-    val ar = stdin.readLine.split(" ").map(_.trim.toLong)
-    val result = aVeryBigSum(ar)
-    printWriter.println(result)
-    printWriter.close()
-  }
+def sum(a: String, b: String, rest: Int = 0, list: List[String] = Nil): Long = {
+  if (a.isEmpty()) return (rest.toString + list.mkString).toLong
+  val f = (a takeRight 1).toInt + (b takeRight 1).toInt + rest
+  if (f < 10) sum(a dropRight 1, b dropRight 1, 0, f.toString :: list)
+  else sum(a dropRight 1, b dropRight 1, f/10, (f%10).toString :: list)
 }
