7cfb108ef5dd4eae32214f2f85357905c3497dc8
lrgfilipe <luis.filipe@linkconsulting.com>
2020-05-02 14:50:33 +0100

    staircase - python

---
+++ easy/Staircase/python-solution.py
@@ -0,0 +1,8 @@
+def staircase(n):
+    print("\n".join([" "*(n-i) + "#"*i for i in range(1, n+1)]))
+
+
+
+
+
+
--- easy/Staircase/scala-solution.scala
+++ easy/Staircase/scala-solution.scala
@@ -1,12 +1,2 @@
-import java.io._
-
-object Solution {
-
-  def staircase(n: Int) =println((1 to n).map{ x => " "*(n-x) + "#"*x }.mkString("\n"))
-
-  def main(args: Array[String]) {
-    val stdin = scala.io.StdIn
-    val n = stdin.readLine.trim.toInt
-    staircase(n)
-  }
-}
+def staircase(n: Int) =
+  println((1 to n).map{ x => " "*(n-x) + "#"*x }.mkString("\n"))
